3bbfc98d38248a9d82ed8a22c423013f8333cdaa
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2023-01-23 20:29:51 +1100

    Fixed token problem

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,12 +21,6 @@ const resolvers = {
   },
 
   Mutation: {
-    addProfile: async (parent, { name, email, password }) => {
-      const profile = await Profile.create({ name, email, password });
-      const token = signToken(profile);
-
-      return { token, profile };
-    },
     login: async (parent, { email, password }) => {
       const profile = await Profile.findOne({ email });
 
@@ -41,6 +35,12 @@ const resolvers = {
       }
 
       const token = signToken(profile);
+      return { token, profile };
+    },
+    addProfile: async (parent, { name, email, password }) => {
+      const profile = await Profile.create({ name, email, password });
+      const token = signToken(profile);
+
       return { token, profile };
     },
     // Set up mutation so a logged in user can only remove their profile and no one else's
