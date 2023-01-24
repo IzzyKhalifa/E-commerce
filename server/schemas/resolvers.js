cd933ea287e45feb4e0b7cfcfa262b6e52810a26
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2023-01-24 20:16:33 +1100

    Added order to index.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Profile } = require('../models');
+const { Profile, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
